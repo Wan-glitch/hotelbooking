3106b79a193ee0c3a2c23f07fc97f87584aefc24
Wan <devzon3@gmail.com>
2023-07-27 16:05:48 +0800

    Masyaaaaalorh

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,21 @@ Route::get('/', function () {
 });
 
 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', [HomeController::class, 'index']);
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+
+
+
 //include csrf token
 //Route::post('/register',[UserController::class, 'register']);
 
